e2b1ae264b243ab0804b67ff6135682e08633803
Angelina <angelina2003t@gmail.com>
2022-06-06 23:25:34 +0300

    tests

--- newsproject/news/tests.py
+++ newsproject/news/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from news.models import *
+from users.models import User
 
-# Create your tests here.
+
+class ArticleModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create()
+        category = Category.objects.create()
+        Article.objects.create(title='title', content='content', author=user, category=category)
+
+    def test_title_label(self):
+        article = Article.objects.get(id=1)
+        field_label = article._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'title')
+
+    def test_title_max_length(self):
+        article = Article.objects.get(id=1)
+        max_length = article._meta.get_field('title').max_length
+        self.assertEqual(max_length, 100)
+
+    def test_author(self):
+        article = Article.objects.get(id=1)
+        field_label = article._meta.get_field('author').verbose_name
+        self.assertEqual(field_label, 'author')
+
+    def test_category(self):
+        article = Article.objects.get(id=1)
+        field_label = article._meta.get_field('category').verbose_name
+        self.assertEqual(field_label, 'category')
+
+    def test_get_absolute_url(self):
+        article = Article.objects.get(id=1)
+        self.assertEqual(article.get_absolute_url(), '/article/id=1')
+
+
+class AuthorModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create()
+        Author.objects.create(user=user, description='description')
+
+    def test_user(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('user').verbose_name
+        self.assertEqual(field_label, 'user')
+
+    def test_description_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('description').verbose_name
+        self.assertEqual(field_label, 'description')
+
+
+class CategoryModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Category.objects.create(name='name')
+
+    def test_name_label(self):
+        category = Category.objects.get(id=1)
+        field_label = category._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
--- newsproject/users/tests.py
+++ newsproject/users/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from users.models import User
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(username='username')
+
+    def test_username_label(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field('username').verbose_name
+        self.assertEqual(field_label, 'username')
+
+    def test_title_max_length(self):
+        user = User.objects.get(id=1)
+        max_length = user._meta.get_field('username').max_length
+        self.assertEqual(max_length, 150)
+
+    def test_email_label(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field('email').verbose_name
+        self.assertEqual(field_label, 'email address')
+
+    def test_date_joined_label(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field('date_joined').verbose_name
+        self.assertEqual(field_label, 'date joined')
